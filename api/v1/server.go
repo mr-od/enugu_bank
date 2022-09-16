334c10121020eac6e62e578f339e5af7c76c3d29
mr-od <chukwuclementod@gmail.com>
2022-09-16 18:56:42 +0900

    auth_token_implementation

--- api/v1/server.go
+++ api/v1/server.go
@@ -1,38 +1,62 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 	db "github.com/oddinnovate/bank_of_enugu/db/sqlc"
+	"github.com/oddinnovate/bank_of_enugu/token"
+	"github.com/oddinnovate/bank_of_enugu/util"
 )
 
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
-	router := gin.Default()
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
+	server.setupRouter()
 
-	// Create User Route
+	return server, err
+}
+
+func (server *Server) setupRouter() {
+
+	router := gin.Default()
+
+	// User Route
 	router.POST("/api/v1/users", server.createUser)
+	router.POST("/api/v1/users/login", server.loginUser)
+
+	authRoutes := router.Group("/").Use(authMiddleware(server.tokenMaker))
 
 	// Accounts Route
-	router.POST("api/v1/accounts", server.createAccount)
-	router.GET("api/v1/accounts/:id", server.getAccount)
-	router.GET("api/v1/accounts", server.listAccounts)
+	authRoutes.POST("api/v1/accounts", server.createAccount)
+	authRoutes.GET("api/v1/accounts/:id", server.getAccount)
+	authRoutes.GET("api/v1/accounts", server.listAccounts)
 
 	// Transfers Route
-	router.POST("api/v1/transfers", server.createTransfer)
+	authRoutes.POST("api/v1/transfers", server.createTransfer)
 
 	server.router = router
-	return server
+
 }
 
 func (server *Server) Start(address string) error {
